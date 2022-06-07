fa5d268710192d8b278aecb50082821313aa5db3
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-07 20:15:27 +0300

    creating superuser

--- insta/models.py
+++ insta/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+from pytz import timezone
+from django.utils import timezone
 
 # Create your models here.
 
 class Post(models.Models):
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, null=True)
+    caption = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
