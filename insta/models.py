a32a4ca4ce1f6a7c90e2d4fd3c520a8cac2dc6c1
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-07 20:38:09 +0300

    posting to admin face

--- insta/models.py
+++ insta/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 # Create your models here.
 
-class Post(models.Models):
+class Post(models.Model):
     author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     image = models.ImageField(blank=True, null=True)
     caption = models.TextField()
