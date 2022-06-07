93fc32e903ecadc1356f152de1d2e14820664c2e
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-07 22:30:56 +0300

    creating views

--- insta/views.py
+++ insta/views.py
@@ -5,6 +5,7 @@ from django.views.generic import (ListView)
 # Create your views here.
 
 class PostListView(ListView):
+    template_name = "insta/post_list.html"
     queryset = Post.objects.all()
     context_object_name = "posts"
     
