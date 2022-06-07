4be9bc024bbfc8364aecd4df344435b2be638357
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-07 20:59:01 +0300

    creating views

--- insta/views.py
+++ insta/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import (ListView)
 
 # Create your views here.
+
+class PostListView(ListView):
+    queryset = Post.objects.all()
+    context_object_name = "posts"
+    
